d9d86391fc7709ad7f92655b4ac064a7d70cbfcb
JHopeCollins <jhc.jss@gmail.com>
2022-08-24 18:03:53 +0100

    poisson

---
+++ introductory_tutorials/poisson/poisson.py
@@ -0,0 +1,33 @@
+import firedrake as fd
+import numpy as np
+import matplotlib.pyplot as plt
+
+mesh = fd.UnitSquareMesh(10, 10, quadrilateral=True)
+
+V = fd.FunctionSpace(mesh, "Q", 1)
+
+x,y = fd.SpatialCoordinate(mesh)
+u_exact = fd.sin(fd.pi*x)*fd.sin(fd.pi*y)
+f = (2*fd.pi**2)*u_exact
+
+u = fd.TrialFunction(V)
+v = fd.TestFunction(V)
+
+a = fd.dot(fd.grad(u), fd.grad(v))*fd.dx
+L = f*v*fd.dx
+
+boundary_ids = (1,2,3,4)
+bcs = fd.DirichletBC(V, 0, boundary_ids)
+
+solver_parameters = {
+    'ksp_type': 'cg',
+    'pc_type': 'none'
+}
+
+uh = fd.Function(V)
+fd.solve( a==L, uh, bcs=bcs, solver_parameters=solver_parameters)
+
+fig, axes = plt.subplots()
+collection = fd.tripcolor(uh, axes=axes)
+fig.colorbar(collection)
+plt.show()
